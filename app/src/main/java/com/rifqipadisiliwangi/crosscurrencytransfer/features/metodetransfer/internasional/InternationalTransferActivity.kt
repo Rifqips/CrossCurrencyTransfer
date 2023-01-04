0d4c6253f1315e6bb0210ad9409e4fba73822193
Rifqi Padi Siliwangi <79899198+Rifqips@users.noreply.github.com>
2023-01-04 14:42:28 +0700

    balik dua commit

--- app/src/main/java/com/rifqipadisiliwangi/crosscurrencytransfer/features/metodetransfer/internasional/InternationalTransferActivity.kt
+++ app/src/main/java/com/rifqipadisiliwangi/crosscurrencytransfer/features/metodetransfer/internasional/InternationalTransferActivity.kt
@@ -4,29 +4,29 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.rifqipadisiliwangi.crosscurrencytransfer.R
-import com.rifqipadisiliwangi.crosscurrencytransfer.data.model.codepicker.CountryData
+import com.rifqipadisiliwangi.crosscurrencytransfer.databinding.ActivityBankInternationalBinding
 import com.rifqipadisiliwangi.crosscurrencytransfer.databinding.ActivityInternationalTransferBinding
-import com.rifqipadisiliwangi.crosscurrencytransfer.features.adapters.codepicker.CountrySpinnerAdapter
 import com.rifqipadisiliwangi.crosscurrencytransfer.features.home.HomeBottomActivity
 
 class InternationalTransferActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityInternationalTransferBinding
-    private var countryData : MutableList<CountryData> = mutableListOf()
-    private lateinit var countryAdapter: CountrySpinnerAdapter
-    private lateinit var countryList: List<CountryData>
-
+    private val images = intArrayOf(R.drawable.ic_ind,
+        R.drawable.ic_us,
+        R.drawable.ic_ausi,
+        R.drawable.ic_sg,
+        R.drawable.ic_jpn,
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityInternationalTransferBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        loadSpinner()
-//        spinnerSetup()
-
+        spinnerSetup()
         binding.ivBack.setOnClickListener {
             startActivity(Intent(this, HomeBottomActivity::class.java))
         }
@@ -37,80 +37,64 @@ class InternationalTransferActivity : AppCompatActivity() {
 
     }
 
-//    private fun spinnerSetup() {
-//    val spinner = binding.spinnerAsal
-//    countryAdapter = CountrySpinnerAdapter(this, countryList)
-//    spinner.adapter = countryAdapter
-//    countryData.addAll(
-//        listOf(
-//            CountryData( R.drawable.ic_ind ),
-//            CountryData( R.drawable.ic_us ),
-//            CountryData( R.drawable.ic_ausi ),
-//            CountryData( R.drawable.ic_sg ),
-//            CountryData( R.drawable.ic_jpn ),
-//        )
-//    )
-//    val adapter = CountrySpinnerAdapter(this, countryData){ data ->
-//        binding.ivAsalTf.setImageResource(0)
-//    }
-//
-//    binding.ivAsalTf?.setFactory({
-//        val imgView = ImageView(applicationContext)
-//        imgView.scaleType = ImageView.ScaleType.CENTER_INSIDE
-//        imgView })
-//
-//    binding.ivTujuanTf?.setFactory({
-//        val imgView = ImageView(applicationContext)
-//        imgView.scaleType = ImageView.ScaleType.CENTER_INSIDE
-//        imgView })
-//
-//
-//    val currenciesResult = resources.getStringArray(R.array.currenciesResult)
-//
-//    if (spinner != null) {
-//        spinner.adapter = adapter
-//
-//            spinner.onItemSelectedListener = object :
-//                AdapterView.OnItemSelectedListener {
-//                override fun onItemSelected(parent: AdapterView<*>,
-//                                            view: View, position: Int, id: Long) {
-//                        binding.ivTujuanTf.setImageResource(images[position])
-//                    binding.etDropAsal.setText(currenciesResult[position])
-//
-//                }
-//
-//                override fun onNothingSelected(parent: AdapterView<*>) {
-//                    // write code to perform some action
-//                }
-//            }
-//        }
-//
-//    }
-//}
+    private fun spinnerSetup() {
+        binding.ivAsalTf?.setFactory({
+            val imgView = ImageView(applicationContext)
+            imgView.scaleType = ImageView.ScaleType.CENTER_INSIDE
+            imgView })
 
-    private fun loadSpinner()    {
-        countryList = listOf()
-        countryData.addAll(
-            listOf(
-                CountryData( R.drawable.ic_ind ),
-                CountryData( R.drawable.ic_us ),
-                CountryData( R.drawable.ic_ausi ),
-                CountryData( R.drawable.ic_sg ),
-                CountryData( R.drawable.ic_jpn ),
-            )
-        )
+        binding.ivTujuanTf?.setFactory({
+            val imgView = ImageView(applicationContext)
+            imgView.scaleType = ImageView.ScaleType.CENTER_INSIDE
+            imgView })
 
-        binding.spinnerAsal.apply {
-            adapter = CountrySpinnerAdapter(applicationContext, countryList)
-            onItemSelectedListener = object : AdapterView.OnItemSelectedListener
-            {
-                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, position: Int, p3: Long)
-                {
-                    countryData[position]
+        val spinner = binding.spinnerAsal
+        val spinnerTujuan = binding.spinnerTujuan
+        val currencies = resources.getStringArray(R.array.currencies)
+        val currenciesResult = resources.getStringArray(R.array.currenciesResult)
+
+        if (spinner != null) {
+            val adapter = ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, currencies,)
+            val adapterResul = ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, currenciesResult,)
+            spinner.adapter = adapter
+            spinner.adapter = adapterResul
+            spinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>,
+                                            view: View, position: Int, id: Long) {
+                    binding.ivAsalTf.setImageResource(images[position])
+                    binding.etDropAsal.setText(currenciesResult[position])
                 }
 
-                override fun onNothingSelected(p0: AdapterView<*>?) {}
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
             }
         }
+
+        if (spinnerTujuan != null) {
+            val adapter = ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, currencies,)
+            val adapterResul = ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, currenciesResult,)
+            spinnerTujuan.adapter = adapter
+            spinnerTujuan.adapter = adapterResul
+            spinnerTujuan.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>,
+                                            view: View, position: Int, id: Long) {
+                    binding.ivTujuanTf.setImageResource(images[position])
+                    binding.etDropTujuan.setText(currencies[position])
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+        }
+
     }
+
 }
