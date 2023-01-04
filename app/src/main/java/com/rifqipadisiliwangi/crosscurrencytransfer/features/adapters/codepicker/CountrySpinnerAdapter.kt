65d5d8f8f198e5e332e6d9f8cd3819924a11d8ac
Rifqi Padi Siliwangi <79899198+Rifqips@users.noreply.github.com>
2023-01-04 16:38:55 +0700

    masih bug di onclick

--- app/src/main/java/com/rifqipadisiliwangi/crosscurrencytransfer/features/adapters/codepicker/CountrySpinnerAdapter.kt
+++ app/src/main/java/com/rifqipadisiliwangi/crosscurrencytransfer/features/adapters/codepicker/CountrySpinnerAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import com.rifqipadisiliwangi.crosscurrencytransfer.data.model.codepicker.CountryData
 import com.rifqipadisiliwangi.crosscurrencytransfer.databinding.ItemCodepickerBinding
 
--- app/src/main/java/com/rifqipadisiliwangi/crosscurrencytransfer/features/metodetransfer/internasional/InternationalTransferActivity.kt
+++ app/src/main/java/com/rifqipadisiliwangi/crosscurrencytransfer/features/metodetransfer/internasional/InternationalTransferActivity.kt
@@ -2,8 +2,6 @@ package com.rifqipadisiliwangi.crosscurrencytransfer.features.metodetransfer.int
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView
 import androidx.appcompat.app.AppCompatActivity
 import com.rifqipadisiliwangi.crosscurrencytransfer.R
 import com.rifqipadisiliwangi.crosscurrencytransfer.data.model.codepicker.CountryData
@@ -16,8 +14,6 @@ class InternationalTransferActivity : AppCompatActivity() {
     private lateinit var binding : ActivityInternationalTransferBinding
     private var countryData : MutableList<CountryData> = mutableListOf()
     private lateinit var countryAdapter: CountrySpinnerAdapter
-    private lateinit var countryList: List<CountryData>
-    private var listData: CountryData? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,25 +44,11 @@ class InternationalTransferActivity : AppCompatActivity() {
             )
         )
 
-        val spiner = binding.spinnerAsal
+        val spinner = binding.spinnerAsal
         countryAdapter = CountrySpinnerAdapter(this, countryData){
             binding.etDropAsal.hint = it?.currencyCode
         }
-        spiner.adapter = countryAdapter
-//        spiner.onItemSelectedListener = this
-
-//        binding.spinnerAsal.apply {
-//            adapter = CountrySpinnerAdapter(applicationContext, countryList)
-//            onItemSelectedListener = object : AdapterView.OnItemSelectedListener
-//            {
-//                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, position: Int, p3: Long)
-//                {
-//                    countryList[position]
-//                }
-//
-//                override fun onNothingSelected(p0: AdapterView<*>?) {}
-//            }
-//        }
+        spinner.adapter = countryAdapter
 
     }
 }
